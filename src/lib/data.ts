d46ae0d4b29b5ef47e5bfb58ec29cac40a65a2f1
Hervé Zossou <hervezossou8@gmail.com>
2025-08-25 12:45:15 +0100

    feat(lib): update file import link

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,17 @@
-import analyticsIcon from "/assets/icons/analytics-icon.png";
-import codeIcon from "/assets/icons/code-icon.png";
-import digitalIcon from "/assets/icons/digital-icon.png";
+/* Featured icon import */
+import analyticsIcon from "../assets/icons/analytics-icon.png";
+import codeIcon from "../assets/icons/code-icon.png";
+import digitalIcon from "../assets/icons/digital-icon.png";
+
+/* Logo images import */
+import dell from "../assets/images/logo-dell.png";
+import lattice from "../assets/images/logo-lattice.png";
+import ncr from "../assets/images/logo-ncr.png";
+import pacificFunds from "../assets/images/logo-pacific-funds.png";
+import rakuten from "../assets/images/logo-rakuten.png";
+import ted from "../assets/images/logo-ted.png";
+import zendesk from "../assets/images/logo-zendesk.png";
+
 
 export const features = [
     {
@@ -24,4 +35,35 @@ export const features = [
         description: "Our advanced code synchronization technology ensures that your data is always up-to-date and accurate, no matter where it's coming from. Whether you're integrating data from multiple sources or working with a team of developers, our synchronization technology makes it easy to collaborate and ensure that your data is consistent and reliable.",
         action: "View code collaboration"
     },
+]
+
+export const logos = [
+    {
+        logo: dell,
+        altText: "Dell logo"
+    },
+    {
+        logo: zendesk,
+        altText: "Zendesk logo"
+    },
+    {
+        logo: rakuten,
+        altText: "Rakuten logo"
+    },
+    {
+        logo: pacificFunds,
+        altText: "PacificFunds logo"
+    },
+    {
+        logo: lattice,
+        altText: "Lattice logo"
+    },
+    {
+        logo: ncr,
+        altText: "NCR logo"
+    },
+    {
+        logo: ted,
+        altText: "TED logo"
+    }
 ]
