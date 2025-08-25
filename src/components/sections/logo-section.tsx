0f7519dc476523811093590d5eb757c734127b39
Hervé Zossou <hervezossou8@gmail.com>
2025-08-25 12:51:07 +0100

    feat(sections): updated font size of title on large screen

--- src/components/sections/logo-section.tsx
+++ src/components/sections/logo-section.tsx
@@ -5,7 +5,7 @@ export const LogoSection = () => {
     return (
         <section className="flex items-center gap-2.5 py-9">
             <div className="flex flex-col items-center gap-10">
-                <h3 className="text-sm text-white/80 text-center leading-[130%] lg:text-lg">
+                <h3 className="text-sm text-white/80 text-center leading-[130%] lg:text-xl">
                     Trusted by teams at over 1,000 of the world’s leading organizations
                 </h3>
                 <div className="w-full flex flex-wrap items-center justify-center gap-7">
