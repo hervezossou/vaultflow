e1ce198054924d939fd71cfcb6b9330653efbb18
Hervé Zossou <hervezossou8@gmail.com>
2025-08-25 10:25:43 +0100

    feat(ui): create icon wrapper component

---
+++ src/components/ui/icon-wrapper.tsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+
+interface IconWrapperProps {
+    icon: string;
+    altText?: string;
+    className: string;
+}
+
+export const IconWrapper = ({ icon, altText, className }: IconWrapperProps) => {
+    return (
+        <div className={clsx("size-12.5 flex items-center justify-center shadow-icon rounded-2xl", className)}>
+            <img src={icon} alt={altText} className="object-cover size-full" />
+        </div>
+    )
+}
