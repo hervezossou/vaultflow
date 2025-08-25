e22028642fcbf9edaefa43dd9b76297d95156935
Hervé Zossou <hervezossou8@gmail.com>
2025-08-25 12:08:38 +0100

    feat(lib): create data file with features fake data

---
+++ src/lib/data.ts
@@ -0,0 +1,27 @@
+import analyticsIcon from "/assets/icons/analytics-icon.png";
+import codeIcon from "/assets/icons/code-icon.png";
+import digitalIcon from "/assets/icons/digital-icon.png";
+
+export const features = [
+    {
+        icon: analyticsIcon,
+        iconAlt: "Analytics icon",
+        title: "Analytics Dashboard",
+        description: "Our Analytics Dashboard provides a clear and intuitive interface for you to easily analyze your data. From customizable graphs to real-time data updates, our dashboard offers everything you need to gain valuable insights.",
+        action: "View dashboard"
+    },
+    {
+        icon: digitalIcon,
+        iconAlt: "Digital icon",
+        title: "Digital Credit Tokens",
+        description: "Reward your customers and incentivize engagement with our innovative digital credit tokens. Our tokens can be customized to match your branding, and are a flexible and scalable way to drive customer loyalty and encourage repeat business.",
+        action: "View tokens"
+    },
+    {
+        icon: codeIcon,
+        iconAlt: "Code icon",
+        title: "Code collaboration",
+        description: "Our advanced code synchronization technology ensures that your data is always up-to-date and accurate, no matter where it's coming from. Whether you're integrating data from multiple sources or working with a team of developers, our synchronization technology makes it easy to collaborate and ensure that your data is consistent and reliable.",
+        action: "View code collaboration"
+    },
+]
