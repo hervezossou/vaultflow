2a1648c0bfdda9f334dcb4f8be33340d5c2bc821
Hervé Zossou <hervezossou8@gmail.com>
2025-08-25 14:43:39 +0100

    feat(app): nested features section into main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { LogoSection } from "./components/sections/logo-section";
+import { FeaturesSection } from "./components/sections/features-section";
 
 export default function App() {
   return (
     <>
-      <main className="bg-dark-purple min-h-screen flex flex-col items-center px-5 lg:px-10">
+      <main className="bg-dark-purple min-h-screen flex flex-col items-center gap-5 px-5 lg:px-10">
         <LogoSection />
+        <FeaturesSection />
       </main>
     </>
   );
