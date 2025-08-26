33af1c56babe87eb16165de0e7edca334432cb84
Hervé Zossou <hervezossou8@gmail.com>
2025-08-26 18:54:45 +0100

    feat(ui): updated button size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 export const Button = ({ label, onAction, variant }: ButtonProps) => {
   const baseStyles =
-    "size-max px-6 py-3 rounded-[60px] transition-all ease-in-out duration-300 cursor-pointer font-medium text-sm lg:text-base";
+    "w-max h-10 flex items-center justify-center px-6 py-3 rounded-[60px] transition-all ease-in-out duration-300 cursor-pointer font-medium text-sm lg:text-base";
   const variantStyles =
     variant === "contained"
       ? "bg-light-gray text-rich-dark hover:opacity-80"
