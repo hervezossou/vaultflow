6f2dc8b5ef87381892f44dcbe551cc38c9cac54a
Hervé Zossou <hervezossou8@gmail.com>
2025-08-30 07:14:48 +0100

    feat(feature-card): create a mapping object for icons shadow style

--- src/components/ui/feature-card.tsx
+++ src/components/ui/feature-card.tsx
@@ -6,6 +6,7 @@ import { ImageWrapper } from "./image-wrapper";
 interface FeaturedCardProps {
     icon: string;
     iconAlt: string;
+    iconStyle?: "analytics" | "digital" | "code";
     title: string;
     description: string;
     action: string;
@@ -15,6 +16,7 @@ interface FeaturedCardProps {
 export const FeatureCard = ({
     icon,
     iconAlt,
+    iconStyle = "analytics",
     title,
     description,
     action,
@@ -23,10 +25,16 @@ export const FeatureCard = ({
     const baseStyles =
         "w-full h-max flex flex-col items-start gap-6 p-15 rounded-[20px] self-stretch lg:flex-row";
 
+    const shadowStyles = {
+        analytics: "shadow-analytics",
+        digital: "shadow-digital",
+        code: "shadow-code",
+    }
+
     return (
         <div className={clsx(baseStyles, "bg-linear-card-gradient shadow-card")}>
             <div className="w-full h-full flex flex-col items-start gap-5">
-                <IconWrapper icon={icon} altText={iconAlt} className="" />
+                <IconWrapper icon={icon} altText={iconAlt} className={shadowStyles[iconStyle]} />
                 <div className="w-full lg:max-w-[458px] flex flex-col items-start gap-2.5">
                     <h2 className="font-medium text-[26px] leading-[130%] text-light-gray">
                         {title}
